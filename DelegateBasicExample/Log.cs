26e59829bf32b8ec5c5233b5c3e0649721da4e10
Can Pehlivan <cpehlivan544@gmail.com>
2024-02-04 12:59:30 +0300

    first commit

---
+++ DelegateBasicExample/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DelegateBasicExample
+{
+    public class Log
+    {
+        private delegate void LogDel(string text);
+        public Log()
+        {
+            
+            var logDelTextToScreen = new LogDel(LogTextToScreen);
+            
+            logDelTextToScreen += LogTextToFile;
+            
+            LogText(logDelTextToScreen);
+        }
+
+        private static void LogText(LogDel logDel)
+        {
+            Console.WriteLine("Please enter your name: ");
+            
+            var name = Console.ReadLine();
+
+            logDel(name);
+        }
+        private void LogTextToScreen(string text)
+        {
+
+            Console.WriteLine($"{DateTime.Now}: {text}");
+
+        }
+
+        private void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            
+            }
+        }
+    }
+}
--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -6,51 +6,14 @@ namespace DelegateBasicExample
 
     class Program
     {
-        delegate void LogDel(string text);
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Log log = new Log();
-
-            LogDel LogTextToScreenDel, LogTextToFileDel;
-
-            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
-
-            LogTextToFileDel = new LogDel(log.LogTextToFile);
-
-            LogDel multiLogDel = LogTextToScreenDel + LogTextToFileDel;
-
-            Console.WriteLine("Please enter your name");
-
-            var name = Console.ReadLine();
-
-            LogText(multiLogDel, name);
-
-            Console.ReadKey();
-        
+             new Log();
         }
 
-        static void LogText(LogDel logDel, string text)
-        {
-            logDel(text);
-        }
 
-    }
 
-    public class Log
-    {
-        public void LogTextToScreen(string text)
-        {
-            Console.WriteLine($"{DateTime.Now}: {text}");
-
-        }
-
-        public void LogTextToFile(string text)
-        {
-            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
-            {
-                sw.WriteLine($"{DateTime.Now}: {text}");
-            }
-        }
     }
 
 }
